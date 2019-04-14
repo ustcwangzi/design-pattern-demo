835beaf857e9637383076b0f623352fb028c14a3
wangzi <ustcwangzi@foxmail.com>
2019-04-14 15:33:03 +0800

    SINGLETON

--- src/main/java/com/wz/singleton/LazyHolderSingleton.java
+++ src/main/java/com/wz/singleton/LazyHolderSingleton.java
@@ -10,6 +10,11 @@ package com.wz.singleton;
 /**
  * <p>静态内部类式单例</p>
  * <p>借鉴了饿汉式，能够保证线程安全，并且因为静态内部类在使用时才加载，不会浪费内存</p>
+ * <p>
+ *     饿汉式方式是只要类被装载就会实例化，没有Lazy-Loading的作用，而静态内部类方式在外部类被装载时并不会立即实例化，
+ *     而是在调用getInstance方法，才会装载LazyHolder类，从而完成LazyHolderSingleton的实例化。
+ *     类的静态属性只会在第一次加载类的时候初始化，JVM帮助我们保证了线程的安全性。
+ * </p>
  *
  * @author wangzi
  */
@@ -18,10 +23,10 @@ public class LazyHolderSingleton {
     }
 
     public static LazyHolderSingleton getInstance() {
-        return LazyHolder.singleton;
+        return LazyHolder.SINGLETON;
     }
 
     private static class LazyHolder {
-        private static LazyHolderSingleton singleton = new LazyHolderSingleton();
+        private static final LazyHolderSingleton SINGLETON = new LazyHolderSingleton();
     }
 }
